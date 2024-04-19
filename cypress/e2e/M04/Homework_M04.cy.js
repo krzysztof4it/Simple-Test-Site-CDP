6fde0f8f2d7664984c06a8eca344ad221a473c5e
Krzysztof <krzysztof4it@gmail.com>
2024-04-19 21:22:56 +0200

    SD-04 - Homework M04

--- cypress/e2e/M04/Homework_M04.cy.js
+++ cypress/e2e/M04/Homework_M04.cy.js
@@ -1,25 +1,58 @@
-describe('Homework_M04', () => {
+describe('Homework_M04 + wzmień po 5 elementów po: ', () => {
 
     beforeEach(()=>{
         cy.visit("https://fabrykatestow.pl/")
     })
 
-    it('Wymień po 5 elementów - ID, CLASS, TEKST, ELEMENT + ATTRYBUT', () => {
+    it('5 elementów - ID', () => {
+
+        // get -> https://docs.cypress.io/api/commands/get
 
         cy.log("ID")
+        cy.get('#menu-item-923');
+        cy.get('#kursy');
+        cy.get('#site-footer');
+        cy.get('#newsletter');
+        cy.get('#custom_html-3');
+
+    })
+
+    it('5 elementów - CLASS', () => {
+
+        cy.get('.brand'); // ikona marki
+        cy.get('.elementor-size-xl');
+        cy.get('.gallery-item') // galeria zaufanych firm
+        cy.get('.elementor-button-text') // buttony np. = "POKAŻ CZEGO SIE NAUCZE"
+        cy.get('.elementor-heading-title') // nagłówki
 
 
-        cy.log("KLASA")
+    })
 
-
-        cy.log("TEKST")
-
-
-        cy.log("ELEMENT + ATTRYBUT")
-
+    it('5 elementów - ELEMENT + ATRYBUT', () => {
+        cy.get('img[width="768"]')
+        cy.get('div[data-id="782c7bd"]')
+        cy.get('input[type="search"]')
+        cy.get('section[data-element_type="section"]')
+        cy.get('a[href="https://fabrykatestow.pl/ciekawostki"]')
+        cy.get('div').and('have.class', 'elementor-widget-container')
 
 
 
 
     })
+
+    it('5 elementów - TEKST', () => {
+        // documentacja: https://docs.cypress.io/api/commands/contains
+
+
+        cy.contains('Bądź na bieżąco')
+        cy.contains('SZKOŁa ONLINE DLA TESTERÓW') // SZKOŁA ONLINE DLA TESTERÓW' celowa mała literka czy znalazłem błąd? :)
+        cy.contains('SZKOŁA ONLINE DLA TESTERÓW', {matchCase:false}) //
+
+        cy.contains('© 2023 Fabryka Testów')
+        cy.get('div').contains('SZ').eq(0)
+        cy.contains('ZERO RYZYKA')
+    })
+
+
 })
