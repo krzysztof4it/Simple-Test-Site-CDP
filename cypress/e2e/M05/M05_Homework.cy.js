1e8322664bd9d4b97ef5d5b0a4c34cfd483cc903
Krzysztof <krzysztof4it@gmail.com>
2024-04-21 23:56:08 +0200

    SD-09 Homework M05

---
+++ cypress/e2e/M05/M05_Homework.cy.js
@@ -0,0 +1,13 @@
+describe('M05_Homework', () => {
+    it('Praca domowa', () => {
+        cy.visit("/")
+        cy.get('.elementor-button').contains('POKAŻ CZEGO SIĘ NAUCZĘ').eq(0).click()
+        cy.contains('Kto nauczy Cię testów automatycznych?').scrollIntoView()
+
+        cy.screenshot( {
+            capture: 'viewport'
+        })
+        //screenshot domyślnie zapisany w folderze screenshots
+
+    })
+})
