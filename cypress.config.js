502617b4b3376bed21d436086e561c40059baab2
Krzysztof <krzysztof4it@gmail.com>
2024-04-21 22:00:59 +0200

    SD-07 Settings test windown

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
+// Documentation: https://docs.cypress.io/guides/references/configuration
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  viewportHeight: 800,
+  viewportWidth:1200,
+
   e2e: {
+    baseUrl: 'https://fabrykatestow.pl/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
   },
 });
