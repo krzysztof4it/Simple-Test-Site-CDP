1acbe20ad9bdd22b62884a9587eb7172c5d2170d
Krzysztof Tlaga <krzysztof.tlaga@marketplanet.pl>
2024-04-23 17:06:48 +0200

    SD-12 API & IFRAME part 2

--- cypress/e2e/M06/API.cy.js
+++ cypress/e2e/M06/API.cy.js
@@ -18,10 +18,29 @@ describe('API', () => {
         cy.request(url).then((response) => {
             const body = response.body
             cy.log(body) // wyświetl body// zawartość => wyświetla - Array[100]
-
             const bodyJSON = JSON.stringify(response.body)
+            expect(response.status).to.eq(200) // spr czy kod odp 200 - OK
             cy.log(bodyJSON) // wyświetla wszystkie dane
         })
+
+        cy.log('Wysyłamy dane do API metodą POST')  // ==> https://jsonplaceholder.typicode.com/posts
+        cy.request(
+            {
+                method: 'POST',
+                url: url,
+                body: JSON.stringify({
+                    title: 'FOO',
+                    userId: '101',
+                    body: 'ESSA essa',
+                }),
+                headers: {'Content-type': 'application/json'} // nagłówek z typem danych for JSON
+            }
+        ).then((response) => {
+            const  bodyPOST = JSON.stringify(response.body)
+            expect(response.status).to.eq(201) // check kode HTTP 201?
+            cy.log(bodyPOST) // show message
+        })
+
 
     })
 })
--- cypress/e2e/M06/IFrame.cy.js
+++ cypress/e2e/M06/IFrame.cy.js
@@ -1,8 +1,23 @@
 // wtyczka https://www.npmjs.com/package/cypress-iframe
 
+//def zmiennych:
+const url = "https://simpletestsite.fabrykatestow.pl/"
+const ifrmeHeder = '#iframe-header'
+const iframe = 'iframe' // only 1 element with iframe one test site
+const iframeButton = '#simpleButton1'
 
 describe('IFrame', () => {
     it('TEST IFRAME ', () => {
-        cy.visit("https://simpletestsite.fabrykatestow.pl")
+        cy.visit(url);
+
+        cy.get(ifrmeHeder).click();
+
+        const iframeTest = cy.get(iframe)
+        .its('0.contentDocument.body')
+        .should('be.visible')
+        .then(cy.wrap)
+
+        iframeTest.find(iframeButton).click()
+
     })
 })
